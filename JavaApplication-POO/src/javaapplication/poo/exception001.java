21cbb6be4ac6465b04f82f71c703b57536f6aa06
deisesilva <kinsk@msn.com>
2021-06-08 11:14:30 -0300

    Commit#4 - Example use try/catch

--- JavaApplication-POO/src/javaapplication/poo/exception001.java
+++ JavaApplication-POO/src/javaapplication/poo/exception001.java
@@ -5,10 +5,31 @@ package javaapplication.poo;
  *
  * @author Deise Kinsk
  */
-   import java.io.IOException;
+ import java.io.IOException;
 public class exception001 {
 
+  void m() throws IOException{
+  throw new IOException("Error!");
+  }
   
-        
+  void n()throws IOException {
+  m();
+  }
+  
+  void p(){
+  try{
+  n();
+  }catch(IOException e){
+  System.out.println("Exception handle");
+  }
+  
+  }
+
+  public static void main(String Arg[]){  
+   exception001 obj=new exception001();  
+   obj.p();  
+   System.out.println("Normal flow...");  
+  } 
 
 }
+   
