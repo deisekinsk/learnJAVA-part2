d55badb91b7b4ec1cd85b6c63cdadb4d35b0982d
deisesilva <kinsk@msn.com>
2021-06-09 20:55:05 -0300

    insert exception finally in new file

---
+++ JavaApplication-POO/src/javaapplication/poo/exception002.java
@@ -0,0 +1,36 @@
+package javaapplication.poo;
+
+/**
+ *
+ * @author Deise Kinsk
+ */
+public class exception002 {
+    public static void main (String args[]){
+        String test = "no";
+        
+        try{
+        System.out.println("Start TRY.");
+        runTheException(test);
+        System.out.println("End TRY.");
+        
+        }catch(personalException ex){
+        System.out.println("Run CATCH");
+        
+        }finally{
+        System.out.println("Run FINALLY");
+        }
+        
+       System.out.println("End of MAIN");
+    }
+    
+    static void runTheException(String test) throws personalException{
+    
+    System.out.println("START the personal exception. ");
+        if("yes".equals(test)){
+            throw new personalException();
+        }
+        System.out.println("END the personal exception.");
+        return;        
+            
+    }
+}
