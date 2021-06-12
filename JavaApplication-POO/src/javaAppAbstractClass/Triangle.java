3ccf644dd1ea2a89631479b3f17b1b993fe779a1
deisesilva <kinsk@msn.com>
2021-06-12 15:42:48 -0300

    Organize project 2

---
+++ JavaApplication-POO/src/javaAppAbstractClass/Triangle.java
@@ -0,0 +1,16 @@
+
+package javaAppAbstractClass;
+
+/**
+ *
+ * @author Deise Kinsk
+ */
+public class Triangle extends AbstractImage2 {
+    
+    public void draw(){
+    System.out.print("  /  \\ ");
+    System.out.print("  ---- ");
+    
+    }
+    
+}
---
+++ JavaApplication-POO/src/javaAppAbstractClass/mainDraw.java
@@ -0,0 +1,19 @@
+
+package javaAppAbstractClass;
+
+/**
+ *
+ * @author Deise Kinsk
+ */
+public class mainDraw {
+    
+    public static void mains (String[] args){
+    Triangle shapeT = new Triangle();
+    Square shapeS = new Square();
+    
+    shapeT.draw();
+    shapeS.draw();
+    
+    }
+    
+}
